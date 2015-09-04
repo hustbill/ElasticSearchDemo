018534bb7804b249dbebaa535bf1b7b2c1ecbe6e
Hua Zhang <billyzhang2010@gmail.com>
2015-09-03 17:21:08 -0700

    add product info  index and query

--- elasticClient.go
+++ elasticClient.go
@@ -5,22 +5,28 @@ import (
     "fmt"
     // "log"
     // "os"
-	"reflect"
+	//"reflect"
 	"time"
 
 	"github.com/olivere/elastic"
 )
 
-type Tweet struct {
-	User     string                `json:"user"`
-	Message  string                `json:"message"`
-	Retweets int                   `json:"retweets"`
-	Image    string                `json:"image,omitempty"`
-	Created  time.Time             `json:"created,omitempty"`
-	Tags     []string              `json:"tags,omitempty"`
-	Location string                `json:"location,omitempty"`
-	Suggest  *elastic.SuggestField `json:"suggest_field,omitempty"`
+type Product struct {
+    Id                          int64       `json:"id"`
+    Name                        string      `json:"name"`
+    Description                 string      `json:"description"`
+    Permalink                   string      `json:"permalink"`
+    TaxCategoryId               int64       `json:"tax_category_id"`
+    ShippingCategoryId          int64       `json:"shipping_category_id"`
+    DeletedAt                   time.Time   `json:"deleted_at"` 
+    MetaDescription             string      `json:"meta_description"`
+    MetaKeywords                string      `json:"meta_keywords"`
+    Position                    int64       `json:"position"`
+    IsFeatured                  bool        `json:"is_featured"`
+    CanDiscount                 bool        `json:"can_discount"`
+    DistributorOnlyMembership   bool        `json:"distributor_only_membership"`
 }
+
 
 func main() {
 
@@ -39,7 +45,7 @@ func main() {
 		// Handle error
 		panic(err)
 	}
-	fmt.Printf("Elasticsearch returned with code %d and version %s", code, info.Version.Number)
+	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
 
 	// Getting the ES version number is quite common, so there's a shortcut
 	esversion, err := client.ElasticsearchVersion("http://127.0.0.1:9200")
@@ -47,18 +53,18 @@ func main() {
 		// Handle error
 		panic(err)
 	}
-	fmt.Printf("Elasticsearch version %s", esversion)
+	fmt.Printf("Elasticsearch version %s\n", esversion)
 
 
 	// Use the IndexExists service to check if a specified index exists.
-	exists, err := client.IndexExists("twitter").Do()
+	exists, err := client.IndexExists("products").Do()
 	if err != nil {
 		// Handle error
 		panic(err)
 	}
 	if !exists {
 		// Create a new index.
-		createIndex, err := client.CreateIndex("twitter").Do()
+		createIndex, err := client.CreateIndex("products").Do()
 		if err != nil {
 			// Handle error
 			panic(err)
@@ -69,38 +75,25 @@ func main() {
 	}
 
 
-	// Index a tweet (using JSON serialization)
-	tweet1 := Tweet{User: "olivere", Message: "Take Five", Retweets: 0}
+	// Index a product (using JSON serialization)
+	product1 := Product{Name: "LINE CORRECTOR", Description: "A cutting-edge treatment with Hyaluronic Bio-Filling Spheres that swell up to 30 times in ", TaxCategoryId: 15}
 	put1, err := client.Index().
-		Index("twitter").
-		Type("tweet").
+		Index("products").
+		Type("product").
 		Id("1").
-		BodyJson(tweet1).
+		BodyJson(product1).
 		Do()
 	if err != nil {
 		// Handle error
 		panic(err)
 	}
-	fmt.Printf("Indexed tweet %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
+	fmt.Printf("Indexed product %s to index %s, type %s\n", put1.Id, put1.Index, put1.Type)
 
-	// Index a second tweet (by string)
-	tweet2 := `{"user" : "olivere", "message" : "It's a Raggy Waltz"}`
-	put2, err := client.Index().
-		Index("twitter").
-		Type("tweet").
-		Id("2").
-		BodyString(tweet2).
-		Do()
-	if err != nil {
-		// Handle error
-		panic(err)
-	}
-    fmt.Printf("Indexed tweet %s to index %s, type %s\n", put2.Id, put2.Index, put2.Type)
-
-	// Get tweet with specified ID
+	
+	// Get product with specified ID
 	get1, err := client.Get().
-		Index("twitter").
-		Type("tweet").
+		Index("products").
+		Type("product").
 		Id("1").
 		Do()
 	if err != nil {
@@ -112,7 +105,7 @@ func main() {
 	}
 
 	// Flush to make sure the documents got written.
-	_, err = client.Flush().Index("twitter").Do()
+	_, err = client.Flush().Index("products").Do()
 	if err != nil {
 		panic(err)
 	}
@@ -120,9 +113,9 @@ func main() {
     // Search with a term query
     termQuery := elastic.NewTermQuery("user", "olivere")
     searchResult, err := client.Search().
-        Index("twitter").   // search in index "twitter"
+        Index("products").   // search in index "products"
         Query(&termQuery).  // specify the query
-        Sort("user", true). // sort by "user" field, ascending
+        Sort("name", true). // sort by "name" field, ascending
         From(0).Size(10).   // take documents 0-9
         Pretty(true).       // pretty print request and response JSON
         Do()                // execute
@@ -135,44 +128,35 @@ func main() {
     // and all kinds of other information from Elasticsearch.
     fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
 
-    // Each is a convenience function that iterates over hits in a search result.
-    // It makes sure you don't need to check for nil values in the response.
-    // However, it ignores errors in serialization. If you want full control
-    // over iterating the hits, see below.
-    var ttyp Tweet
-    for _, item := range searchResult.Each(reflect.TypeOf(ttyp)) {
-        if t, ok := item.(Tweet); ok {
-            fmt.Printf("Tweet by %s: %s\n", t.User, t.Message)
-        }
-    }
+
     // TotalHits is another convenience function that works even when something goes wrong.
-    fmt.Printf("Found a total of %d tweets\n", searchResult.TotalHits())
+    fmt.Printf("Found a total of %d products\n", searchResult.TotalHits())
 
     // Here's how you iterate through results with full control over each step.
     if searchResult.Hits != nil {
-        fmt.Printf("Found a total of %d tweets\n", searchResult.Hits.TotalHits)
+        fmt.Printf("Found a total of %d products\n", searchResult.Hits.TotalHits)
 
         // Iterate through results
         for _, hit := range searchResult.Hits.Hits {
             // hit.Index contains the name of the index
 
             // Deserialize hit.Source into a Tweet (could also be just a map[string]interface{}).
-            var t Tweet
-            err := json.Unmarshal(*hit.Source, &t)
+            var p Product
+            err := json.Unmarshal(*hit.Source, &p)
             if err != nil {
                 // Deserialization failed
             }
 
-            // Work with tweet
-            fmt.Printf("Tweet by %s: %s\n", t.User, t.Message)
+            // Work with product
+            fmt.Printf("Product by %s: %s\n", p.Name, p.Description)
         }
     } else {
         // No hits
-        fmt.Print("Found no tweets\n")
+        fmt.Print("Found no products\n")
     }
 
     // Delete the index again
-    _, err = client.DeleteIndex("twitter").Do()
+    _, err = client.DeleteIndex("products").Do()
     if err != nil {
         // Handle error
         panic(err)
